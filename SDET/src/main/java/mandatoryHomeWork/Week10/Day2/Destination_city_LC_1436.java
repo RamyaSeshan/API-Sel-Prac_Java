224258e84d3cc9b8b9e1c5256c7079a64453d3ff
ramyaseshan01@gmail.com <ramyaseshan01@gmail.com>
2023-10-05 12:12:36 +0530

    Oct 03 HW Commit

--- SDET/src/main/java/mandatoryHomeWork/Week10/Day2/Destination_city_LC_1436.java
+++ SDET/src/main/java/mandatoryHomeWork/Week10/Day2/Destination_city_LC_1436.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -11,6 +12,19 @@ import java.util.Set;
 
 import org.junit.Test;
 
+
+// https://leetcode.com/problems/destination-city/submissions/
+
+/*
+ *  Time Complexity O [ Nlog N ] ; Space Complexity - O[N ] 
+ * Psuedo Code 
+ *   1.Iterate thur list and get the dest [ list.get(1) ] and src  [list.get(0) ]
+ *    -->   in a Map ,add string value asKey and dest occurance and values 
+ *    --> in the same map , decremtn the src values 
+ *  2.iterate thur  Map entry set
+ *    -->if any of the entity has value 1 , return its key 
+ *   
+ */
 public class Destination_city_LC_1436 {
 	
 	 public String destCity1(List<List<String>> paths) {
@@ -43,14 +57,26 @@ public class Destination_city_LC_1436 {
 	        
 	    }
 
- public String destCity(List<List<String>> paths) {
+ public String destCity2(List<List<String>> paths) {
 		 
 		 Map<String ,Integer> values = new HashMap<String ,Integer>() ;
 		 StringBuffer src = new StringBuffer();
 		 StringBuffer dest = new StringBuffer();
 		 
+		 
+		 for (List<String> list : paths) {			 
+			 
+			 dest =  dest.append(list.get(1));
+			 src =  src.append(list.get(0));
+			 values.put(dest.toString(), values.getOrDefault(dest.toString(), 0) +1) ;
+			 values.put(src.toString(), values.getOrDefault(src.toString(), 0) -1) ;
+			 
+			 dest.setLength(0);
+			 src.setLength(0);
+			
+	     	}
 		
-		 for (int i = 0; i < paths.size(); i++) 
+		/* for (int i = 0; i < paths.size(); i++) 
 		   {
 			 dest =  dest.append(paths.get(i).get(1));
 			 src =  src.append(paths.get(i).get(0));
@@ -59,10 +85,9 @@ public class Destination_city_LC_1436 {
 			 
 			 dest.setLength(0);
 			 src.setLength(0);
-	     	}
+	     	}*/
 	
 		 
-		 System.out.println(values);
 		for(Map.Entry<String ,Integer> result : values.entrySet()) {
 			
 			if(result.getValue() == 1) { return result.getKey()  ; }
@@ -71,6 +96,24 @@ public class Destination_city_LC_1436 {
 		 return null;
 	        
 	    } 
+ 
+ public String destCity(List<List<String>> paths)  //Approach II 
+ {
+	 Set<String> destn = new HashSet<String>();
+		List<String> res = new ArrayList<String>();
+		
+		for (List<String> source : paths) 
+		{
+			if (!destn.contains(source.get(0)))
+				destn.add(source.get(0));		
+		}
+		for (List<String> destination : paths) 
+		{
+			if (!destn.contains(destination.get(1)))
+				res.add(destination.get(1));
+		}
+		return res.iterator().next();
+ }
 	 
 	 @Test
 	 public void test1()
---
+++ SDET/src/main/java/mandatoryHomeWork/Week10/Day2/Fair_candy_swap_LC_888.java
@@ -0,0 +1,84 @@
+package mandatoryHomeWork.Week10.Day2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class Fair_candy_swap_LC_888 {
+	
+	//https://leetcode.com/problems/fair-candy-swap/
+	
+	/*
+	 * PSUEDO code
+	 *   1.Get total sum of aliceCandies and Bob Candies 
+	 *   2.Put one of the person candies ina set 
+	 *   3.Find the diffrence of aliceCandies &  Bob Candies  and divide it by 2 [ because if we have diff 5 candies , then we need to 
+	 *     balce the share by giving 2.5 to both of them ]
+	 *  4.iterate thur the person candies who is NOT added to set 
+	 *    --> get the temp value as current candie in the box + diff  [ diff found in syep 3 ]
+	 *    --> check if this temp value exists in set , if so return current candie  and value that matches in set 
+	 *    
+	 */
+	
+	 public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+		
+		 int bobTotal = 0 , aliceTotal = 0 ,temp ;
+		 Set <Integer> set = new HashSet<Integer>() ;
+		 
+		 for (int i : bobSizes) {
+			 bobTotal += i;
+		}
+		 
+		for (int j : aliceSizes) {
+			aliceTotal += j ;
+			set.add(j);
+		} 
+		
+		System.out.println(set);
+		int diff = (aliceTotal -bobTotal  )/2;
+		System.out.println(diff);
+		
+		for (int bobCandie : bobSizes) {
+			
+			temp = bobCandie + diff;
+			if(set.contains(temp))
+			{
+				return new int[] {temp , bobCandie };
+			}		
+				}		
+		 return null;
+	        
+	    }
+
+	 @Test
+	 public void Test1()
+	 {
+		 int[] aliceSizes = {1,1}, bobSizes = {2,2} ;
+		 int[] fairCandySwap = fairCandySwap( aliceSizes,  bobSizes) ;
+		 for (int i : fairCandySwap) {
+			  System.out.println(i);
+		   }
+	 }
+	 
+	 @Test
+	 public void Test2()
+	 {
+		 int[] aliceSizes = {1,2}, bobSizes = {2,3} ;
+		 int[] fairCandySwap = fairCandySwap( aliceSizes,  bobSizes) ;
+		 for (int i : fairCandySwap) {
+			  System.out.println(i);
+		   }
+	 }
+	 
+	 
+	 @Test
+	 public void Test3()
+	 {
+		 int[] aliceSizes = {2}, bobSizes = {1,3} ;
+		 int[] fairCandySwap = fairCandySwap( aliceSizes,  bobSizes) ;
+		 for (int i : fairCandySwap) {
+			  System.out.println(i);
+		   }
+	 }
+}
