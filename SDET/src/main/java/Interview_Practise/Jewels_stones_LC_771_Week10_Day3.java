ac95a1018522f78d10ef12b2097eb77a238551c5
ramyaseshan01@gmail.com <ramyaseshan01@gmail.com>
2024-02-25 22:18:17 +0530

    Feb 25 Commit

--- SDET/src/main/java/Interview_Practise/Jewels_stones_LC_771_Week10_Day3.java
+++ SDET/src/main/java/Interview_Practise/Jewels_stones_LC_771_Week10_Day3.java
@@ -1,13 +1,15 @@
 package Interview_Practise;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
 
 public class Jewels_stones_LC_771_Week10_Day3 
 {
-	
+
 	@Test
 	public void test1()
 	{
@@ -17,7 +19,7 @@ public class Jewels_stones_LC_771_Week10_Day3
 		System.out.println( " numJewelsInStones test1    " + numJewelsInStones);
 	}
 
-	
+
 	@Test
 	public void test2()
 	{
@@ -26,27 +28,47 @@ public class Jewels_stones_LC_771_Week10_Day3
 		int numJewelsInStones = numJewelsInStones( jewels,  stones) ;
 		System.out.println( " numJewelsInStones  test2 " + numJewelsInStones);
 	}
-	public int numJewelsInStones(String jewels, String stones) 
+	public int numJewelsInStones_1(String jewels, String stones)  // Using Map 
 	{
-	
+
 		Map< Character , Integer> map = new HashMap<Character , Integer>() ;
 		int total = 0 ;
-		
+
 		for (int i = 0; i < stones.length(); i++) // O[N] 
 		{
-		  map.put(stones.charAt(i), map.getOrDefault(stones.charAt(i), 0) + 1 )	;
-		 
+			map.put(stones.charAt(i), map.getOrDefault(stones.charAt(i), 0) + 1 )	;
+
 		}
-		
+
 		//System.out.println(map);
-		
+
 		for (int i = 0; i < jewels.length(); i++)   // O [N] 
 		{
 			if ( map.containsKey( jewels.charAt(i)) )  total= total + map.get(jewels.charAt(i)) ;
 		}
-		
+
 		return total ;
 	}
-	
-	
+
+	public int numJewelsInStones(String jewels, String stones)  // Uasing Set 
+	{
+
+		Set < Character> set = new HashSet< Character >() ;
+		int total = 0 ;
+
+		for (Character character : jewels.toCharArray()) 
+		{
+			set.add( character ) ; 
+		}
+
+		for (Character character : stones.toCharArray() ) 
+		{
+			if ( set.contains( character)  ) total = total+ 1;
+		}
+
+
+		return total;
+
+	}
+
 }
