cc41ad67ec44a4e8958ad39026bec14696304a2f
ramyaseshan01@gmail.com <ramyaseshan01@gmail.com>
2024-03-13 15:03:38 +0530

    Mar 13 Commit

---
+++ SDET/src/main/java/Interview_Practise/ArraywithValuesRepeatedTwice_LC_217.java
@@ -0,0 +1,68 @@
+package Interview_Practise;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class ArraywithValuesRepeatedTwice_LC_217 
+{
+	@Test
+	public void test1()
+	{
+		int[] nums = {1,2,3,1 };
+		boolean containsDuplicate = containsDuplicate(nums) ;
+		System.out.println( " test 1 " + containsDuplicate );
+
+	}
+
+
+	@Test
+	public void test2()
+	{
+		int[] nums = {1,2,3,4};
+		boolean containsDuplicate = containsDuplicate(nums) ;
+		System.out.println( " test 2 " + containsDuplicate );
+
+	}
+
+	@Test
+	public void test3()
+	{
+		int[] nums = {1,1,1,3,3,4,3,2,4,2};
+		boolean containsDuplicate = containsDuplicate(nums) ;
+		System.out.println( " test 3  " + containsDuplicate );
+
+	}
+	public boolean containsDuplicate_1(int[] nums) {
+
+		Set<Integer> set = new HashSet<Integer>() ;
+
+		for (Integer integer : nums) 
+		{
+			if  ( ! set.contains(integer))  set.add(integer) ;
+			else return true;
+
+		}
+		return false;		
+
+	}
+	
+	
+	public boolean containsDuplicate(int[] nums) 
+	{
+		Arrays.sort(nums);
+		
+		for (int i = 0; i < nums.length - 1; i++) 
+		{
+			if ( nums[i] == nums[i + 1])  return true ;
+		}
+		
+		
+		return false;
+		
+	}
+	
+	
+}
