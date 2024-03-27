d745703cfdd24a0397ad129ed72ffb67e8dd2ed5
ramyaseshan01@gmail.com <ramyaseshan01@gmail.com>
2024-03-27 14:41:49 +0530

    Mar 27 Commit

---
+++ SDET/src/main/java/Interview_Practise/AbeforeB_LC_2124_WKIIICW.java
@@ -0,0 +1,57 @@
+package Interview_Practise;
+
+import org.junit.Test;
+
+public class AbeforeB_LC_2124_WKIIICW 
+{
+	// https://leetcode.com/problems/check-if-all-as-appears-before-all-bs/description/
+
+	@Test
+	public void test1()
+	{
+		String s = "aaabbb" ;
+		boolean checkString = checkString( s );
+		System.out.println(" test1  --- >  " + checkString);		
+	}
+
+
+	@Test
+	public void test2()
+	{
+		String s = "abab" ;
+		boolean checkString = checkString( s );
+		System.out.println(" test 2  --- >  " + checkString);		
+	}
+
+	@Test
+	public void test3()
+	{
+		String s = "bbb" ;
+		boolean checkString = checkString( s );
+		System.out.println(" test 3  --- >  " + checkString);		
+	}
+	
+	@Test
+	public void test4()
+	{
+		String s = "aa" ;
+		boolean checkString = checkString( s );
+		System.out.println(" test 4  --- >  " + checkString);		
+	}
+
+	public boolean checkString(String s) 
+	{
+		int indexOf = s.indexOf("b");
+		if (indexOf == -1 ) return false ;
+		int i =  indexOf + 1 ;
+
+
+		while ( i < s.length())
+		{
+			if (s.charAt(i) == 'a') return false;
+			i++;
+		}
+
+		return true;
+	}
+}
