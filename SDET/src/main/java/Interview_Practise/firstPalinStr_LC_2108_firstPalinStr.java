8c11a1ad76638e1505afc02449591389ee9c0a46
ramyaseshan01@gmail.com <ramyaseshan01@gmail.com>
2024-03-28 17:04:06 +0530

    Mar 28 Commit

---
+++ SDET/src/main/java/Interview_Practise/firstPalinStr_LC_2108_firstPalinStr.java
@@ -0,0 +1,59 @@
+package Interview_Practise;
+
+import org.junit.Test;
+
+public class firstPalinStr_LC_2108_firstPalinStr
+{
+	// https://leetcode.com/problems/find-first-palindromic-string-in-the-array/description/ 
+	// took 157 ms  , need to retry 
+
+	@Test
+	public void test1()
+	{
+		String[] words =  { "abc","car","ada","racecar","cool" };
+		String firstPalindrome = firstPalindrome( words) ;
+		System.out.println( "test1 ---" +firstPalindrome);
+	}
+	
+	@Test
+	public void test2()
+	{
+		String[] words =  { "notapalindrome","racecar" };
+		String firstPalindrome = firstPalindrome( words) ;
+		System.out.println( "test2 ---" +firstPalindrome);
+	}
+	
+	@Test
+	public void test3()
+	{
+		String[] words =  { "def","ghi" };
+		String firstPalindrome = firstPalindrome( words) ;
+		System.out.println( "test3 ---" +firstPalindrome);
+	}
+	
+
+	public String firstPalindrome(String[] words) {
+		
+		boolean flag ;
+
+		for (int i = 0; i < words.length; i++) 
+		{			
+			flag = revStr(words[i])  ;
+			if (flag) return  words[i];
+		}
+		return "";
+	}
+
+	public boolean revStr(String word) 
+	{
+      int left =0  , right = word.length() -1 ;
+      
+      while ( left < right ) 
+      {
+    	  if(word.charAt(left) != word.charAt(right)) return false ;
+    	  left ++ ; right --;
+      }
+        return true ;
+	}	
+
+}
